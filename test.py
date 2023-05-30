0013fc7217fd1c1bb3be1d4d301a65ca729570c6
sarahk46 <sarahk46@uw.edu>
2023-05-29 20:51:21 -0700

    Adding the test files

---
+++ test.py
@@ -0,0 +1,20 @@
+"""
+Name: Sarah Khan, Cindy Qian, and Junhee Park
+Group: P077
+This file tests functions that are implemented by data_cleaning.py
+"""
+
+
+import data_cleaning
+from cse163_utils import assert_equals
+def test_get_unique_word_percentage() -> None:
+    """
+    Tests data_cleaning.py's get_unique_word_percentage().
+    """
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
